9599d3db8ae8ae8dbad36a5cdd3444d1d748b04e
Vladimir-Romanov131 <92376566+Vladimir-Romanov131@users.noreply.github.com>
2021-10-30 22:57:52 +0300

    Update Program.cs

--- инд1/Program.cs
+++ инд1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace инд1 {
     class Program {
         static void Main(string[] args) {
@@ -8,33 +7,60 @@ namespace инд1 {
             Console.WriteLine("Введите секунды");
             uint s = uint.Parse(Console.ReadLine());
             uint M;
+            uint c;
             uint S;
-            if(m<=9 ){
+            uint Ms = m * 60;
+            uint a = Ms + s;
+            c = a / 3600;
+            m = (a - ( c * 3600)) / 60;
+            s = (a - (c * 3600) - (m * 60));
+            if(c<=9){
+                if(m<=9 ){
                 M = m;
                 if(s<=9){
                     S = s;
-                    Console.WriteLine($"0{m}:0{s}");
+                    Console.WriteLine($"0{c}:0{m}:0{s}");
                 }
                 else if(s>9){
                     S = s;
-                    Console.WriteLine($"0{m}:{s}");
-
-                }
-                
-                
+                    Console.WriteLine($"0{c}:0{m}:{s}");
+                }             
             }
             else if (m>0){
                 M = m;
                 if(s<=9){
                     S = s;
-                    Console.WriteLine($"{m}:0{s}");
+                    Console.WriteLine($"0{c}:{m}:0{s}");
                 }
                 else if(s>9){
                     S = s;
-                    Console.WriteLine($"{m}:{s}");
-
+                    Console.WriteLine($"0{c}:{m}:{s}");
                 }
-
+            }
+            }
+            else if(c>9 || c<=24){
+                if(m<=9 ){
+                M = m;
+                if(s<=9){
+                    S = s;
+                    Console.WriteLine($"{c}:0{m}:0{s}");
+                }
+                else if(s>9){
+                    S = s;
+                    Console.WriteLine($"{c}:0{m}:{s}");
+                }               
+            }
+            else if (m>0){
+                M = m;
+                if(s<=9){
+                    S = s;
+                    Console.WriteLine($"{c}:{m}:0{s}");
+                }
+                else if(s>9){
+                    S = s;
+                    Console.WriteLine($"{c}:{m}:{s}");
+                }
+            }
             }
         }
     }
